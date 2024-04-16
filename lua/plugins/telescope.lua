2f67058d19daea1f943d624e8a01be7128f0e3c7
Matthew Tift <matthew.tift@lullabot.com>
2024-04-16 12:26:52 -0500

    Update telescope shortcuts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,8 +4,9 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function ()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, {})
     end
   },
   {
