9aff32434eea2b909a51cce9db1548cd366ffdc7
Matthew Tift <matthew.tift@lullabot.com>
2024-04-28 11:15:43 -0500

    Reduce the size of neovim on the splash screen

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,30 +2,13 @@ return {
   "goolord/alpha-nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
   },
 
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.startify")
-
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
-
+    dashboard.section.header.val = { 'Neovim' }
     alpha.setup(dashboard.opts)
   end,
 }
