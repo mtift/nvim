e3ef249e971b719f3e46bbb5c490737a30b22ae7
Matthew Tift <matthew.tift@lullabot.com>
2024-04-28 10:57:30 -0500

    Be explicit about comment mappings

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,9 +2,19 @@
   'numToStr/Comment.nvim',
   config = function ()
     require('Comment').setup({
-      options = {
-        lazy = false,
-      },
-    })
-  end
+       options = {
+         lazy = false,
+         --- LHS of toggle mappings in NORMAL mode
+         toggler = {
+           line = 'gcc',
+           block = 'gbc',
+         },
+         --- LHS of operator-pending mappings in NORMAL and VISUAL mode
+         opleader = {
+           line = 'gc',
+           block = 'gb',
+         },
+       },
+     })
+   end
 }
