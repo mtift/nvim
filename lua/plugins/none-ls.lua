5268a752758e4d9521ced8e877c63726f2d67081
Matthew Tift <matthew.tift@lullabot.com>
2024-04-28 11:44:09 -0500

    Reconfigure phpcs to use phpcs.xml

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -11,20 +11,25 @@ return {
         -- null_ls.builtins.diagnostics.phpstan.with({
         --   prefer_local = "vendor/bin",
         -- }),
-        --
+
+        -- A manual (NOT recommended) way to enforce Drupal coding standards:
         -- In .zshrc/.bashrc:
         --   export PATH="$HOME/.local/share/nvim/mason/bin:$PATH"
         -- Install Drupal Coder:
         --   composer global require drupal/coder:8.3.13
         -- Configure the Mason phpcs to use Coder:
         --   phpcs --config-set installed_paths ~/.config/composer/vendor/drupal/coder/coder_sniffer
-        -- TODO: Configure phpcs to find phpcs.xml
+        -- And then add this line below:
+        --   extra_args = { "--standard=Drupal" }
+        --
+        -- However, the better way is to first include core-dev:
+        --   ddev composer require --dev drupal/core-dev
+        -- And then include a phpcs.xml file, such as:
+        --   https://github.com/Lullabot/drainpipe/blob/main/drainpipe-dev/scaffold/phpcs.xml
+        --
+        -- See https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md#phpcs
         null_ls.builtins.diagnostics.phpcs.with({
           prefer_local = "vendor/bin",
-          extra_args = { "--standard=Drupal" }
-        }),
-        null_ls.builtins.formatting.phpcbf.with({
-          extra_args = { "--standard=Drupal" }
         }),
         null_ls.builtins.formatting.phpcbf.with({
           prefer_local = "vendor/bin",
