df591b09f5a1d7d32777463455cd173f60079951
Matthew Tift <matthew.tift@lullabot.com>
2024-05-15 12:57:57 -0500

    Update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
   config = function ()
     local config = require("nvim-treesitter.configs")
     config.setup({
+      ensure_installed = { "lua", "xml", "http", "json", "graphql" },
       -- Install parsers whenever new languages are encountered (helpful!)
       auto_install = true,
       indent = { enable = true },
