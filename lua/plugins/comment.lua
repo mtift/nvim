aa714494b166289813d0d05720dbf90f90509bac
Matthew Tift <matthew.tift@lullabot.com>
2024-05-28 07:55:42 -0500

    Remove the breakpoints that conflict with the breakpoint keymaps

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -6,12 +6,10 @@ return {
       --- Toggle mappings in NORMAL mode
       toggler = {
         line = '<leader>cc',
-        block = '<leader>bc',
       },
       --- Toggle mappings in NORMAL and VISUAL mode
       opleader = {
         line = '<leader>c',
-        block = '<leader>b',
       },
     })
   end
